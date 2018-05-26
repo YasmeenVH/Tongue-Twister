8eacdb0e3d1ba6e319c9956a67f82ddcd08ca9b4
adeeb27 <adeeb.amjad@mail.mcgill.ca>
2018-05-26 18:05:58 -0400

    Website working with Flask

--- app.py
+++ app.py
@@ -3,15 +3,26 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     #return "Hello tongue twisters!"
     name="Peter Piper Pan"
     tongueTwisters = ["she sells sea shells", "peter piper pan likes pokemon"]
     return render_template("index.html", name=name, list=tongueTwisters)
 
-@app.route("/anotherPage")
-def hello1():
-    return "This is another page."  
+@app.route("/selectPage1")
+def selectPage1():
+    #return "This is another page."  
+    return render_template("selectTTPage1.html")
+
+@app.route("/selectPage2")
+def selectPage2():
+    #return "This is another page."  
+    return render_template("selectTTPage2.html")
+
+@app.route("/selectPage3")
+def selectPage3():
+    #return "This is another page."  
+    return render_template("selectTTPage3.html")
 
 if __name__ == "__main__":
     app.run()
