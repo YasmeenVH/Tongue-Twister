b287db1638e70d69932e235f518727bc4254bc3c
adeeb27 <adeeb.amjad@mail.mcgill.ca>
2018-05-26 18:15:10 -0400

    Fixed Typo

--- app.py
+++ app.py
@@ -4,24 +4,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    #return "Hello tongue twisters!"
+    
     name="Peter Piper Pan"
     tongueTwisters = ["she sells sea shells", "peter piper pan likes pokemon"]
     return render_template("index.html", name=name, list=tongueTwisters)
 
 @app.route("/selectPage1")
 def selectPage1():
-    #return "This is another page."  
+     
     return render_template("selectTTPage1.html")
 
 @app.route("/selectPage2")
 def selectPage2():
-    #return "This is another page."  
+     
     return render_template("selectTTPage2.html")
 
 @app.route("/selectPage3")
 def selectPage3():
-    #return "This is another page."  
+    
     return render_template("selectTTPage3.html")
 
 if __name__ == "__main__":
